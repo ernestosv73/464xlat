a568535cb8a6746ae7690542349d4c38efda0c76
Ernesto Sánchez <154641197+ernestosv73@users.noreply.github.com>
2024-09-09 00:09:05 -0300

    Update server.sh

--- configs/server.sh
+++ configs/server.sh
@@ -2,9 +2,11 @@ apk add openrc
 sleep 1
 apk add bind 
 sleep 1
+apk add dhclient
+sleep 1
 apk add apache2
 touch /run/openrc/softlevel
-sleep 1
+#sleep 1
 /etc/rc.d/apache2 stop
 /etc/init.d/nginx stop
 /usr/sbin/httpd
